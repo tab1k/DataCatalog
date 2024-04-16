aa073fcab8a9a02ecd42f4d27e98ce8025ce9c0c
Карбаев Табигат <tabigat@MacBook-Pro-Karbaev.local>
2024-04-16 20:19:47 +0500

    added searching

--- app/views.py
+++ app/views.py
@@ -28,46 +28,36 @@ class BusinessGlossaryViewSet(viewsets.ModelViewSet):
 
 
 
+from itertools import chain
+from django.db.models import Q, Value, CharField
+from rest_framework.generics import ListAPIView
+from .models import Passport, PassportDataStructure, BusinessGlossary
+from .serializers import SearchResultsSerializer
+
+
 class SearchResultsAPIView(ListAPIView):
     serializer_class = SearchResultsSerializer
-    filter_backends = [SearchFilter]
-    search_fields = ['name', 'owner__username', 'passportdatastructure__passport__name', 'passportdatastructure__table_name', 'businessglossary__name', 'businessglossary__termin']
 
     def get_queryset(self):
         query = self.request.query_params.get('search', '')
 
-        # Create an empty queryset
-        queryset = Passport.objects.none()
-
         # Filter passports
-        passports = Passport.objects.filter(name__icontains=query).annotate(
-            name_annotated=F('name'),
-            owner_annotated=F('owner__username')
-        ).values('name_annotated', 'owner_annotated')
+        passports = Passport.objects.filter(
+            Q(name__icontains=query) | Q(owner__username__icontains=query)
+        ).annotate(model_name=Value('Passport', output_field=CharField()))
 
         # Filter passport data structures
-        structures = PassportDataStructure.objects.filter(table_name__icontains=query).annotate(
-            passport_annotated=F('passport__name'),
-            table_name_annotated=F('table_name')
-        ).values('passport_annotated', 'table_name_annotated')
+        structures = PassportDataStructure.objects.filter(
+            table_name__icontains=query
+        ).annotate(model_name=Value('PassportDataStructure', output_field=CharField()))
 
         # Filter business glossaries
-        glossaries = BusinessGlossary.objects.filter(name__icontains=query).annotate(
-            name_annotated=F('name'),
-            termin_annotated=F('termin')
-        ).values('name_annotated', 'termin_annotated')
+        glossaries = BusinessGlossary.objects.filter(
+            Q(name__icontains=query) | Q(termin__icontains=query)
+        ).annotate(model_name=Value('BusinessGlossary', output_field=CharField()))
 
-        # Combine querysets
-        for passport in passports:
-            queryset |= Passport.objects.filter(name=passport['name_annotated'], owner__username=passport['owner_annotated'])
-
-        for structure in structures:
-            queryset |= PassportDataStructure.objects.filter(passport__name=structure['passport_annotated'], table_name=structure['table_name_annotated'])
-
-        for glossary in glossaries:
-            queryset |= BusinessGlossary.objects.filter(name=glossary['name_annotated'], termin=glossary['termin_annotated'])
-
-        return queryset.distinct()
+        # Combine and return the results
+        return list(chain(passports, structures, glossaries))
 
 
 
